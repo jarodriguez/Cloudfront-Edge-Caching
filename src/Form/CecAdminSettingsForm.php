69da15c3ff29f830dd1d05b90b1154690690350f
jarodriguez <jarodriguez@emergya.com>
2017-03-06 19:09:17 +0000

    update form

--- src/Form/CecAdminSettingsForm.php
+++ src/Form/CecAdminSettingsForm.php
@@ -2,106 +2,117 @@
 
 namespace Drupal\cloudfront_edge_caching\Form;
 
-use Drupal\Core\Form\FormBase;
-use Drupal\Core\Form\FormStateInterface;
 use Drupal\Core\Form\ConfigFormBase;
-use Drupal\Core\Config\ConfigFactoryInterface;
+use Drupal\Core\Form\FormStateInterface;
 
 /**
- * Implements an example form.
+ * Configure settings for Cloudfront credentials
  */
 class CecAdminSettingsForm extends ConfigFormBase {
 
-    /**
-     * Constructor for ComproCustomForm.
-     *
-     * @param \Drupal\Core\Config\ConfigFactoryInterface $config_factory
-     * The factory for configuration objects.
-     */
-    public function __construct(ConfigFactoryInterface $config_factory) {
-        parent::__construct($config_factory);
-    }
+  /**
+   * Returns a unique string identifying the form.
+   *
+   * @return string
+   * The unique string identifying the form.
+   */
+  public function getFormId() {
+    return 'cec_admin_settings';
+  }
 
-    /**
-     * Returns a unique string identifying the form.
-     *
-     * @return string
-     * The unique string identifying the form.
-     */
-    public function getFormId() {
-        return 'cec.adminsettingsform';
-    }
+  /**
+   * {@inheritdoc}
+   */
+  protected function getEditableConfigNames() {
+    return [
+      'cec.settings',
+    ];
+  }
 
-    /**
-     * Form constructor.
-     *
-     * @param array $form
-     * An associative array containing the structure of the form.
-     * @param \Drupal\Core\Form\FormStateInterface $form_state
-     * The current state of the form.
-     *
-     * @return array
-     * The form structure.
-     */
-    public function buildForm(array $form, FormStateInterface $form_state) {
+  /**
+   * Form constructor.
+   *
+   * @param array $form
+   * An associative array containing the structure of the form.
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   * The current state of the form.
+   *
+   * @return array
+   * The form structure.
+   */
+  public function buildForm(array $form, FormStateInterface $form_state) {
 
-        // Region.
-        $form['cec_region'] = [
-            '#type' => 'textfield',
-            '#title' => t('Region'),
-            '#size' => 10,
-            '#maxlength' => 128,
-            '#description' => $this->t('Ej: us-east-1'),
-        ];
+    $config = $this->config('cec.settings');
 
-        // Key.
-        $form['cec_key'] = [
-            '#type' => 'textfield',
-            '#title' => t('Key'),
-            '#size' => 50,
-            '#maxlength' => 128,
-            '#description' => $this->t('Ej: EOjWGh6Keft9czeNkmHsa1aMcrhYukxdlIXRayDt'),
-        ];
+    // Region.
+    $form['cec_region'] = [
+      '#type' => 'textfield',
+      '#title' => t('Region'),
+      '#default_value' => $config->get('cec_region'),
+      '#size' => 10,
+      '#maxlength' => 128,
+      '#description' => $this->t('Ej: us-east-1'),
+    ];
 
-        // Secret.
-        $form['cec_secret'] = [
-            '#type' => 'textfield',
-            '#title' => t('Secret'),
-            '#size' => 20,
-            '#maxlength' => 128,
-            '#description' => $this->t('Ej: AHIAJF6JNSRJRVNSDOKA'),
-        ];
+    // Key.
+    $form['cec_key'] = [
+      '#type' => 'textfield',
+      '#title' => t('Key'),
+      '#default_value' => $config->get('cec_key'),
+      '#size' => 50,
+      '#maxlength' => 128,
+      '#description' => $this->t('Ej: EOjWGh6Keft9czeNkmHsa1aMcrhYukxdlIXRayDt'),
+    ];
 
-        // Distribution ID.
-        $form['cec_distribution_id'] = [
-            '#type' => 'textfield',
-            '#title' => t('Distribution ID'),
-            '#size' => 20,
-            '#maxlength' => 128,
-            '#description' => $this->t('Ej: E206SWIPUZ2Z48'),
-        ];
+    // Secret.
+    $form['cec_secret'] = [
+      '#type' => 'textfield',
+      '#title' => t('Secret'),
+      '#default_value' => $config->get('cec_secret'),
+      '#size' => 20,
+      '#maxlength' => 128,
+      '#description' => $this->t('Ej: AHIAJF6JNSRJRVNSDOKA'),
+    ];
 
-        $form['actions']['submit'] = array(
-            '#type' => 'submit',
-            '#value' => $this->t('Save'),
-            '#button_type' => 'primary',
-        );
+    // Distribution ID.
+    $form['cec_distribution_id'] = [
+      '#type' => 'textfield',
+      '#title' => t('Distribution ID'),
+      '#default_value' => $config->get('cec_distribution_id'),
+      '#size' => 20,
+      '#maxlength' => 128,
+      '#description' => $this->t('Ej: E206SWIPUZ2Z48'),
+    ];
 
-        return $form;
-    }
+    $form['actions']['submit'] = array(
+      '#type' => 'submit',
+      '#value' => $this->t('Save'),
+      '#button_type' => 'primary',
+    );
 
-    public function validateForm(array &$form, FormStateInterface $form_state) {
-    }
+    return parent::buildForm($form, $form_state);
+  }
 
-    /**
-     * Form submission handler.
-     *
-     * @param array $form
-     * An associative array containing the structure of the form.
-     * @param \Drupal\Core\Form\FormStateInterface $form_state
-     * The current state of the form.
-     */
-    public function submitForm(array &$form, FormStateInterface $form_state) {
+  public function validateForm(array &$form, FormStateInterface $form_state) {
+  }
 
-    }
+  /**
+   * Form submission handler.
+   *
+   * @param array $form
+   * An associative array containing the structure of the form.
+   * @param \Drupal\Core\Form\FormStateInterface $form_state
+   * The current state of the form.
+   */
+  public function submitForm(array &$form, FormStateInterface $form_state) {
+    // Retrieve the configuration
+    $this->config('cec.settings')
+      // Set the submitted configuration setting
+      ->set('cec_region', $form_state->getValue('cec_region'))
+      ->set('cec_key', $form_state->getValue('cec_key'))
+      ->set('cec_secret', $form_state->getValue('cec_secret'))
+      ->set('cec_distribution_id', $form_state->getValue('cec_distribution_id'))
+      ->save();
+    parent::submitForm($form, $form_state);
+  }
 }
